ca346d4c0c68b81ac927f8e68c86bc506d2a3532
thesyntaxdude <prince.akakpo@icloud.com>
2025-08-03 22:54:38 +0000

    feat: Implemented winner announcement
    Now the game can announce the result of each round.

--- scripts/app.js
+++ scripts/app.js
@@ -9,13 +9,13 @@
     let count = 0;
     let avatar = "";
     let inverseAvatar = "";
+
     function checkMove(e, turnMessageField) {
       boxes.forEach((box) => {
         if (!box.textContent) {
           count++;
         }
       });
-
       if (count % 2 !== 0) {
         avatar = "X";
         inverseAvatar = "O";
@@ -28,19 +28,21 @@
       if (e.target.classList.contains("box") && !e.target.textContent) {
         e.target.textContent = avatar;
       }
+
       checkResult();
+
       count = 0;
     }
     return checkMove;
   }
 
-  // scoreboard variables
+  // scoreboard
   const playerX = document.querySelector("#player-x-score");
   const playerO = document.querySelector("#player-o-score");
   const draw = document.querySelector("#draw-score");
   const scoreBoard = generateScoreBoard();
   function generateScoreBoard() {
-    const Score = {
+    const Score = JSON.parse(localStorage.getItem("scores")) || {
       playerXScore: 0,
       playerOScore: 0,
       drawScore: 0,
@@ -57,6 +59,9 @@
       Score.drawScore++;
       draw.textContent = Score.drawScore;
     }
+
+    localStorage.setItem("scores", JSON.stringify(Score));
+
     return { playerXWins, playerOWins, drawMatch };
   }
 
@@ -77,64 +82,111 @@
       box1.textContent === box3.textContent
     ) {
       avatar = box1.textContent;
-      return announceResults(avatar);
+      return announceResults(avatar, "win");
     } else if (
       box4.textContent &&
       box4.textContent === box5.textContent &&
       box4.textContent === box6.textContent
     ) {
       avatar = box4.textContent;
-      return announceResults(avatar);
+      return announceResults(avatar, "win");
     } else if (
       box7.textContent &&
       box7.textContent === box8.textContent &&
       box7.textContent === box9.textContent
     ) {
       avatar = box7.textContent;
-      return announceResults(avatar);
+      return announceResults(avatar, "win");
     } else if (
       box1.textContent &&
       box1.textContent === box4.textContent &&
       box1.textContent === box7.textContent
     ) {
       avatar = box1.textContent;
-      return announceResults(avatar);
+      return announceResults(avatar, "win");
     } else if (
       box2.textContent &&
       box2.textContent === box5.textContent &&
       box2.textContent === box8.textContent
     ) {
       avatar = box2.textContent;
-      return announceResults(avatar);
+      return announceResults(avatar, "win");
     } else if (
       box3.textContent &&
       box3.textContent === box6.textContent &&
       box3.textContent === box9.textContent
     ) {
       avatar = box3.textContent;
+      return announceResults(avatar, "win");
     } else if (
       box1.textContent &&
       box1.textContent === box5.textContent &&
       box1.textContent === box9.textContent
     ) {
       avatar = box1.textContent;
-      return announceResults(avatar);
+      return announceResults(avatar, "win");
     } else if (
       box3.textContent &&
       box3.textContent === box5.textContent &&
       box3.textContent === box7.textContent
     ) {
       avatar = box3.textContent;
-      return announceResults(avatar);
+      return announceResults(avatar, "win");
+    }
+    checkDraw();
+  }
+
+  function announceResults(avatar, state) {
+    const messageField = document.querySelector("#results>p");
+    const resultsBoard = document.querySelector("#results-board");
+    const gameBoard = document.querySelector("#gameboard");
+    gameBoard.classList.add("hide");
+    resultsBoard.classList.add("active");
+    switch (state) {
+      case "win":
+        if (avatar === "X") {
+          scoreBoard.playerXWins();
+        } else {
+          scoreBoard.playerOWins();
+        }
+        messageField.textContent = `PLAYER ${avatar} WINS!`;
+        break;
+      case "draw":
+        scoreBoard.drawMatch();
+        messageField.textContent = `IT'S A DRAW!`;
+        break;
+      default:
+        messageField.textContent = "";
     }
   }
 
-  function announceResults(avatar) {
-    console.log(`PLAYER ${avatar} WINS!`);
-    // show a modal dialog box here so user can't interact with game after declaring winner
-    // allow user to restart game by resetting the boxes.
+  // announce draw
+  function checkDraw() {
+    const boxes = document.querySelectorAll(".box");
+    let count = 1;
+    const avatar = "";
+    boxes.forEach((box) => {
+      if (!box.textContent) {
+        count++;
+      }
+    });
+    if (count === 1) {
+      return announceResults(avatar, "draw");
+    }
   }
 
-  // add a function to update the scoreboard.
+  // restart game
+  const restartBtn = document.querySelector("#restart-btn");
+  restartBtn.addEventListener("click", restart);
+
+  function restart() {
+    const boxes = document.querySelectorAll(".box");
+    boxes.forEach((box) => (box.textContent = ""));
+    const resultsBoard = document.querySelector("#results-board");
+    const gameBoard = document.querySelector("#gameboard");
+    gameBoard.classList.remove("hide");
+    resultsBoard.classList.remove("active");
+  }
+
   // save the score object to localStorage.
 })();
